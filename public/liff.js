969193d9aed99ba539fb8496a8703152202995ce
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2019-11-15 00:46:28 +0900

    Refactor liff.js

--- public/liff.js
+++ public/liff.js
@@ -1,12 +1,4 @@
-window.onload = function (e) {
-  // init で初期化。基本情報を取得。
-  // https://developers.line.me/ja/reference/liff/#initialize-liff-app
-  liff.init(function (data) {
-    initializeApp(data);
-  });
-};
-
-function initializeApp(data) {
+const initializeApp = (data) => {
   document.getElementById('languagefield').textContent = data.language;
   document.getElementById('viewtypefield').textContent = data.context.viewType;
   document.getElementById('useridfield').textContent = data.context.userId;
@@ -14,3 +6,11 @@ function initializeApp(data) {
   document.getElementById('roomidfield').textContent = data.context.roomId;
   document.getElementById('groupidfield').textContent = data.context.groupId;
 }
+
+window.onload = (e) => {
+  // init で初期化。基本情報を取得。
+  // https://developers.line.me/ja/reference/liff/#initialize-liff-app
+  liff.init((data) => {
+    initializeApp(data);
+  });
+};
